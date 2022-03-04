3254e3af09bdfc2075c6c42c61096a5c6a82290c
dgaida01 <dean.gaida@hotmail.com>
2022-03-04 14:18:34 -0500

    updated to outlook

--- flask_app/models/msg.py
+++ flask_app/models/msg.py
@@ -3,14 +3,14 @@ from flask_app import app, session
 
 app.config['DEBUG'] = True
 app.config['TESTING'] = False
-app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
+app.config['MAIL_SERVER'] = 'smtp.office365.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 # app.config['MAIL_DEBUG'] = True
-app.config['MAIL_USERNAME'] = 'apikey'
-app.config['MAIL_PASSWORD'] = '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'
-app.config['MAIL_DEFAULT_SENDER'] = 'platomic.info@gmail.com'
+app.config['MAIL_USERNAME'] = 'PlatomicAdm@outlook.com'
+app.config['MAIL_PASSWORD'] = 'PbyD&A2021'
+app.config['MAIL_DEFAULT_SENDER'] = 'PlatomicAdm@outlook.com'
 app.config['MAIL_MAX_EMAILS'] = None
 # app.config['MAIL_SUPPRESS_SEND'] = False
 app.config['MAIL_ASCII_ATTACHMENTS'] = False
@@ -23,7 +23,7 @@ class AutoCom:
 
     @classmethod
     def sendMessage(cls, subject,to, body):
-        msg = Message(subject,recipients=[to],sender='platomic.info@gmail.com')
+        msg = Message(subject,recipients=[to],sender='PlatomicAdm@outlook.com')
         msg.html = body
         mail.send(msg)
     
