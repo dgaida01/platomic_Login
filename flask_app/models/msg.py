20eddae35570e32dc9f83b0d3eec10a1fd17ae3b
dgaida01 <dean.gaida@hotmail.com>
2022-03-04 13:11:04 -0500

    message update

--- flask_app/models/msg.py
+++ flask_app/models/msg.py
@@ -3,13 +3,13 @@ from flask_app import app, session
 
 app.config['DEBUG'] = True
 app.config['TESTING'] = False
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 # app.config['MAIL_DEBUG'] = True
-app.config['MAIL_USERNAME'] = 'platomic.info@gmail.com'
-app.config['MAIL_PASSWORD'] = 'PbyD&A2021'
+app.config['MAIL_USERNAME'] = 'apikey'
+app.config['MAIL_PASSWORD'] = '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'
 app.config['MAIL_DEFAULT_SENDER'] = 'platomic.info@gmail.com'
 app.config['MAIL_MAX_EMAILS'] = None
 # app.config['MAIL_SUPPRESS_SEND'] = False
@@ -23,7 +23,7 @@ class AutoCom:
 
     @classmethod
     def sendMessage(cls, subject,to, body):
-        msg = Message(subject,recipients=[to])
+        msg = Message(subject,recipients=[to],sender='platomic.info@gmail.com')
         msg.html = body
         mail.send(msg)
     
